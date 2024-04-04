75250e95df87a32fab209e91bf16ed281ce7e803
Udara San <udarassanjeewa@gmail.com>
2024-04-05 01:48:08 +0530

    thread states

---
+++ ThreadStates/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * @TimeStamp $DATE $TIME
+ * @ProjectDetails $PROJECT_NAME
+ * @Author $USER
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
