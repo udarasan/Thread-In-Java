995dad23e0baf5a15a99a0a2ca9cffd50f293583
Udara San <udarassanjeewa@gmail.com>
2024-04-04 20:59:35 +0530

    step 2 - use anonymous runnable

--- Lambda-Expression/src/main/java/org/example/JavaThread.java
+++ Lambda-Expression/src/main/java/org/example/JavaThread.java
@@ -5,37 +5,35 @@ package org.example;
  * @ProjectDetails $PROJECT_NAME
  * @Author $USER
  */
-//we use this class to only implement from runnable interface
-class One implements Runnable {
-    public void run(){
-        for (int i=1;i<=5;i++) {
-            System.out.println("Thread One");
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-}
-//we use this class to only implement from runnable interface
-class Two implements Runnable {
-    public void run(){
-        for (int i=1;i<=5;i++) {
-            System.out.println("Thread Two");
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-}
 public class JavaThread {
     public static void main(String[] args) {
 
-        Runnable obj1=new One();
-        Runnable obj2=new Two();
+        Runnable obj1=new Runnable() {
+            @Override
+            public void run() {
+                for (int i=1;i<=5;i++) {
+                    System.out.println("Thread One");
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        };
+        Runnable obj2=new Runnable() {
+            @Override
+            public void run() {
+                for (int i=1;i<=5;i++) {
+                    System.out.println("Thread Two");
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        };
 
         Thread t1=new Thread(obj1);
         Thread t2=new Thread(obj2);
