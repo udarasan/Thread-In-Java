fa96ab676cf4163410ae2944c48fcf6917519d9b
Udara San <udarassanjeewa@gmail.com>
2024-04-04 21:53:47 +0530

    step 1 - initial setup

---
+++ Synchronized-Keyword/src/main/java/org/example/JavaThreadSync.java
@@ -0,0 +1,21 @@
+package org.example;
+
+/**
+ * @TimeStamp $DATE $TIME
+ * @ProjectDetails $PROJECT_NAME
+ * @Author $USER
+ */
+class Calculation{
+    int num;
+    public void increment(){
+        num++;
+    }
+}
+public class JavaThreadSync {
+    public static void main(String[] args) {
+
+        Calculation c=new Calculation();
+        c.increment();
+        System.out.println("COUNT : "+c.num);
+    }
+}
