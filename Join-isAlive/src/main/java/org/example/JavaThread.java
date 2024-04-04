4848953b503c0c027efd9c9fe4f5d1adca9dc5cc
Udara San <udarassanjeewa@gmail.com>
2024-04-04 21:25:07 +0530

    step 3 - wait until completion of t1 and t 2 thread's jobs

--- Join-isAlive/src/main/java/org/example/JavaThread.java
+++ Join-isAlive/src/main/java/org/example/JavaThread.java
@@ -6,7 +6,7 @@ package org.example;
  * @Author $USER
  */
 public class JavaThread {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         Thread t1=new Thread(() -> {
             for (int i=1;i<=5;i++) {
@@ -38,6 +38,12 @@ public class JavaThread {
         }
         t2.start();
 
+        // In Java, the join() method is a part of the Thread class. It allows one thread to wait for the completion of
+        // another thread. When you call the join() method on a thread, the current thread will pause its execution until
+        // the thread it is joining with completes its execution.
+
+        t1.join();
+        t2.join();
 
         //main thread print this line
         System.out.println("Bye");
