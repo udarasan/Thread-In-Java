6262b23643404c1c820972be93e83cfd86abe4bd
Udara San <udarassanjeewa@gmail.com>
2024-04-04 21:31:34 +0530

    step 1 - initial setup

---
+++ Thread-Priority/src/main/java/org/example/JavaThread.java
@@ -0,0 +1,50 @@
+package org.example;
+
+/**
+ * @TimeStamp $DATE $TIME
+ * @ProjectDetails $PROJECT_NAME
+ * @Author $USER
+ */
+public class JavaThread {
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread t1=new Thread(() -> {
+            for (int i=1;i<=5;i++) {
+                System.out.println("Thread One");
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        Thread t2=new Thread(() -> {
+            for (int i=1;i<=5;i++) {
+                System.out.println("Thread Two");
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+
+
+        t1.start();
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        //main thread print this line
+        System.out.println("Bye");
+
+
+    }
+}
